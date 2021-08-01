980f05750270ba16a0ae05b61cc2e54866141039
JonasMoura-J <jonasmourat9090@gmail.com>
2021-08-01 15:21:54 -0300

    Exercicio de filtro

--- funcionarios/funcionarios.js
+++ funcionarios/funcionarios.js
@@ -3,5 +3,14 @@ const axios = require('axios')
 
 axios.get(url).then(response => {
     const funcionarios = response.data
-    console.log(funcionarios)
+
+    const mulheresChinesas = funcionarios.filter(f => f.genero == "F" && f.pais == "China")
+
+    const menorSalario = Math.min(mulheresChinesas.map(({
+        salario
+    }) => salario));
+
+    const mulher = mulheresChinesas.find(d => d.salario == menorSalario)
+    console.log(mulher)
 })
+
