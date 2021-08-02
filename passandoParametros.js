8dfc79905a0f59cbf565f8bee9dddf52e5d7f637
JonasMoura-J <jonasmourat9090@gmail.com>
2021-08-02 20:42:43 -0300

    Passando pareametros entre modulos

---
+++ passandoParametros.js
@@ -0,0 +1,5 @@
+module.exports = function(...nomes){
+    return nomes.map(nome => `Boa semana ${nome}!`)
+}
+
+//como passar um parâmentro para outro módulo
---
+++ passandoParametrosCliente.js
@@ -0,0 +1,3 @@
+//colocando parâmetros na função require('./passandoParametros')('Ana', 'Lucas', 'João')
+const saudacoes = require('./passandoParametros')('Ana', 'Lucas', 'João')
+console.log(saudacoes)
