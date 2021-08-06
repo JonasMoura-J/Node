3fe0af9390e75f12a36431f38f490066a4b0cf1c
JonasMoura-J <jonasmourat9090@gmail.com>
2021-08-05 21:14:42 -0300

    Entrada e saída com process

---
+++ entradaESaida.js
@@ -0,0 +1,15 @@
+const anonimo = process.argv.indexOf('-a') !== -1
+
+if(anonimo){
+    process.stdout.write('Fala Anônimo!\n')
+    process.exit()
+} else {
+    process.stdout.write('Informe seu nome: ')
+    
+    process.stdin.on('readable', data => {
+        const nome = data.toString().replace('\n', '')
+        process.stdout.write(`Fala ${nome}!!\n`)
+        process.exit()
+
+    })
+}
